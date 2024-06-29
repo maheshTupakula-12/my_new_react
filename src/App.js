1d4103df4eea6f6ec3f2f60996bbb38e46661ff6
mahesh <makeshtupakula123@gmail.com>
2024-06-29 20:12:43 +0530

    last_commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { useState,useEffect } from 'react';
 
-function App() {
+function App() { 
+  useEffect(()=>{
+    fetch('https://mypro-backend.onrender.com/patient')
+    .then((response)=>response.text())
+    .then((data)=>{
+      console.log(data)
+    }).catch(err=>console.log(err))
+  },[])
+  // const addData = async()=>{
+  //   fetch('')
+  // }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <table>
+            <tr>
+              <th>
+                  Name_Of_Recruiter
+              </th>
+              <th>
+                  Candidate_Added
+              </th>
+              <th>
+                Candidate_Id
+              </th>
+              <th>
+                Time_Stamp
+              </th>
+            </tr>
+        </table>
+      </div>
+    </>
+   
   );
 }
 
